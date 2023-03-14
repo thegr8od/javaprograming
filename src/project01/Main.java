99ef7acbec31f9aa8fb87e443d4425e0f709f815
test <test@DESKTOP-3QRN1H3>
2023-03-14 11:03:01 +0900

    first

--- src/project01/Main.java
+++ src/project01/Main.java
@@ -3,8 +3,9 @@ package project01;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int j = 100;
+		int i = 0;
+		i = (j%2 == 0)?10:20;
+		System.out.println(i);
 
-	}
-
-}
+}}
